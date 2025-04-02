fde9ea982ea2ccbfcdda299b68138a15decadbea
dz24 <rubidxx.dz@gmail.com>
2025-04-03 08:56:12 +0900

    update

--- inftools/tistools/flow2.py
+++ inftools/tistools/flow2.py
@@ -5,9 +5,9 @@ import typer
 typer.main.get_command_name = lambda name: name
 
 def calc_flow2(
+    plot: Annotated[str, typer.Option("-plot", help="Plot the flow for those paths, string of spaced idxes")]="",
     toml: Annotated[str, typer.Option("-toml", help="The .toml input file defining the orderparameter")] = "infretis.toml",
     log: Annotated[str, typer.Option("-log", help="The .log file to read path numbers")] = "sim.log",
-    out: Annotated[bool, typer.Option("-out", help="The output of the analysis")] = False,
     ):
     import numpy as np
     import tomli
@@ -16,7 +16,7 @@ def calc_flow2(
     with open(toml, "rb") as toml_file:
         config = tomli.load(toml_file)
     n_ensembles = len(config["simulation"]["interfaces"])
-    flow_map = {i: {"path": [i], "ens": [i], "step": [0]} for i in range(n_ensembles)}
+    flow_map = {i: {"path": [i], "ens": [i], "step": [0], "cmd": ["init"]} for i in range(n_ensembles)}
     rep_hist = {i: i for i in range(n_ensembles)} # path : replica
 
     step = 0
@@ -30,40 +30,52 @@ def calc_flow2(
 
             # accepted 0 <->
             if len(set(srip)) == 6:
-                print(
-                po_m, po_p = rip[10], rip[11]
-                pn_m, pn_p = rip[13], rip[14]
+                po_m, po_p = rip[2], rip[3]
+                pn_m, pn_p = rip[4], rip[5]
+                ens_m, ens_p = rip[0], rip[1]
 
-                assert flow_map[0]["path"][-1] == po_m
-                assert flow_map[1]["path"][-1] == po_m
-                assert flow_map[0]["ens"][-1] == 1
-                assert flow_map[1]["ens"][-1] == 0
+                replica_m = rep_hist[po_m]
+                replica_p = rep_hist[po_p]
+                # swap
+                rep_hist[pn_p] = rep_hist[po_m]
+                rep_hist[pn_m] = rep_hist[po_p]
 
-                flow_map[0]["path"].append(rip[13])
-                flow_map[0]["ens"].append(0)
-                flow_map[1]["path"].append(rip[14])
-                flow_map[1]["ens"].append(1)
+                del rep_hist[po_m]
+                del rep_hist[po_p]
+
+                flow_map[replica_m]["path"].append(pn_p)
+                flow_map[replica_m]["ens"].append(ens_p)
+                flow_map[replica_m]["step"].append(step)
+                flow_map[replica_p]["path"].append(pn_m)
+                flow_map[replica_p]["ens"].append(ens_m)
+                flow_map[replica_p]["step"].append(step)
 
             # accepted sh/wf
             elif len(set(srip)) == 3:
-                replica = rep_hist[rip[1]]
-                rep_hist[rip[2]] = rep_hist[rip[1]]
-                if rip[1] in rep_hist:
-                    del rep_hist[rip[1]]
+                ens = rip[0]
+                p_o = rip[1]
+                p_n = rip[2]
 
-                print("hehe", srip)
-                print(replica, rep_hist)
-                flow_map[replica]["path"].append(rip[2])
-                flow_map[replica]["ens"].append(rip[0])
+                replica = rep_hist[p_o]
+                rep_hist[p_n] = rep_hist[p_o]
+
+                del rep_hist[p_o]
+
+                flow_map[replica]["path"].append(p_n)
+                flow_map[replica]["ens"].append(ens)
                 flow_map[replica]["step"].append(step)
 
-                # path = rep_hist[
-                print("b", int(rip[0]), int(rip[1]), int(rip[2]))
-                print(line)
-                print(flow_map[replica])
-                print("")
-            # if step == 7:
 
-            #     print("bear")
-            #     exit()
+    pn_numbs = []
+    if plot != "":
+        for ens in range(n_ensembles):
+            s1 = len(set(pn_numbs))
+            s2 = len(set(flow_map[ens]["path"]))
+            s3 = len(set(pn_numbs + flow_map[ens]["path"]))
+            assert s1 + s2 == s3
 
+            plt.plot(flow_map[ens]["step"], flow_map[ens]["ens"], marker = "o", markersize = 5)
+            plt.title(f"replica {ens:03.0f}")
+            plt.show()
+
+    return flow_map
