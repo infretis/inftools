99e415dced59044dacb59171c32ca6cce0cfffa2
Lukas Baldauf <lukas.baldauf@ntnu.no>
2025-04-16 11:11:37 +0200

    add test_estimate_binless_interfaces

--- test/test_estimate_interfaces/test_estimate_binless_interfaces.py
+++ test/test_estimate_interfaces/test_estimate_binless_interfaces.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 import pathlib
 
@@ -6,51 +7,47 @@ from inftools.misc.infinit_helper import estimate_binless_interface_positions
 HERE = pathlib.Path(__file__).parent
 
 
-# orderp values for testing
+# nice orderp values for testing
 X = np.array([i for i in range(10)])
-X1 = np.array([0] + [1 for i in range(4)] +  [i+2 for i in range(5)])
+# bit harder with closely spaced x (but not duplicate values)
+X2 = np.array([0] + [1 + 1e-4*i for i in range(4)] +  [i+2 for i in range(5)])
 
 # local crossing probability of 0.5
 PL0 = 0.5
 # pcross for first test
 P0 = np.array([PL0**i for i in range(10)])
-
-# pcross for second test with large drop
+# pcross for second test with a large drop
 P1 = np.array([1, 1, 1] + [PL0**(i+3) for i in range(7)])
 
-def test_estimate_binless_pcross():
-    """Test that we can place interfaces in an exact test case"""
+@pytest.mark.parametrize("x,p,pL", [
+    (X, P0, 0.50),
+    (X, P0, 0.49),
+    (X, P0, 0.51),
+    (X, P1, 0.55),
+    (X, P1, 0.25),
+    (X2,P1, 0.79),
+    (X2,P1, 0.19),
+    ])
+def test_estimate_binless_pcross(x, p, pL):
+    """Test that we can place interfaces exactly for some test cases.
 
-    # contains orderp and pcross with large drop
-    interfaces = estimate_binless_interface_positions(X, P0, 0.5)
+    It is assumed that x does not contain duplicates, as returned by
+    `inf get_path_weights -outP pcross.txt`.
+    """
+    # last interface is not added
+    interfaces, pL_used = estimate_binless_interface_positions(x, p, pL)
+    expected_ptot_at_interfaces = [pL_used**(i) for i in range(len(interfaces))]
+    # get actual Ptot at interfaces by interpolating X vs log(P)
+    ptot_at_interfaces = np.exp(np.interp(interfaces, x, np.log(p)))
+    assert np.allclose(ptot_at_interfaces, expected_ptot_at_interfaces)
     # uncomment to see plots of pcross and estimated interfacess
-    import matplotlib.pyplot as plt
-    plt.plot(X, P0, marker="x",lw=2.5)
-    for i, intf in enumerate(interfaces):
-        plt.axvline(intf,c="k")
-        plt.axhline(PL0**(i+1),c="k")
-    plt.yscale("log")
-    plt.show()
-    assert np.allclose(X, interfaces)
-
-
-def test_large_drop_binless_pcross():
-    """Test that we place interfaces correctly with a large drop in pcross."""
-    # contains orderp and pcross with large drop
-    p_here=0.50
-    interfaces = estimate_binless_interface_positions(X, P1, p_here)
-    print(interfaces)
-    import matplotlib.pyplot as plt
-    plt.plot(X, P1, marker="x",lw=2.5)
-    for i, intf in enumerate(interfaces):
-        plt.axvline(intf,c="k")
-        plt.axhline(p_here**(i+1),c="k")
-    plt.yscale("log")
-    plt.show()
-    #assert np.allclose(X, interfaces)
-
-#def test_multiple_points_binless_pcross():
-
-
-test_large_drop_binless_pcross()
-#test_estimate_binless_pcross()
+    #import matplotlib.pyplot as plt
+    #plt.plot(x, p, marker="x",lw=2.5)
+    #for i, (intf,ptot) in enumerate(zip(interfaces, expected_ptot_at_interfaces)):
+    #    plt.axvline(intf,c="k")
+    #    plt.axhline(ptot,c="k")
+    #plt.yscale("log")
+    #plt.plot(interfaces, ptot_at_interfaces)
+    #plt.plot(interfaces, expected_ptot_at_interfaces)
+    #print("expected Ptot at interfaces", expected_ptot_at_interfaces)
+    #plt.show()
